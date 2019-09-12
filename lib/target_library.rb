64df28384c7ee544daa5efa6c42bfab31a0501f3
Joel Helbling <joel@joelhelbling.com>
2019-09-12 13:22:33 +0000

    change 'target' env var to 'MEDIA_STAGING_PATH'

--- lib/target_library.rb
+++ lib/target_library.rb
@@ -2,6 +2,7 @@ require_relative 'base_library'
 
 class TargetLibrary < BaseLibrary
   def initialize
-    @path = ENV["MEDIA_TARGET_PATH"]
+    @path = ENV["MEDIA_STAGING_PATH"]
+    raise "You must provide a MEDIA_STAGING_PATH environment variable!" unless @path
   end
 end
