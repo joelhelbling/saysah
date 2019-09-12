25a4f79d7a002d501a7981b2c7ce3773d96a1ccf
Joel Helbling <joel@joelhelbling.com>
2019-09-12 11:01:21 +0000

    eliminates the need for a 'removes' param
    The FaithComesByHearingRenamer used to use a `renames` parameter so that
    it could know which part of each filename to remove.  However, this is
    as easily accomplished by a relatively simple regex homed in the renamer
    itself, so the need to have that parameter for each resource from Faith
    Comes By Hearing is not necessary.

--- lib/renamers/faith_comes_by_hearing_renamer.rb
+++ lib/renamers/faith_comes_by_hearing_renamer.rb
@@ -4,7 +4,7 @@ class FaithComesByHearingRenamer < BaseRenamer
   def new_name(file, params)
     book, chapter = file.name \
       .gsub(/^[AB]\d{2}___/, '')
-      .gsub(/_*#{params.removes}\.mp3/, '')
+      .gsub(/_*[0-9A-Z]+\.mp3/, '')
       .split(/_/).reverse
     "#{target.path}/#{indexer.next}_#{params.name}_#{book}_#{chapter.to_i}.mp3"
   end
